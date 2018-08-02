c2a6a864bc6d6c1fc7c7a92a8b78d02e6d126397
Shuping LIU <lsp.lhh@gmail.com>
2018-08-02 23:59:24 +0800

    problem 111

---
+++ 111.minimum-depth-of-binary-tree.js
@@ -0,0 +1,50 @@
+/*
+ * [111] Minimum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (33.88%)
+ * Total Accepted:    229.4K
+ * Total Submissions: 677K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its minimum depth.
+ * 
+ * The minimum depth is the number of nodes along the shortest path from the
+ * root node down to the nearest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its minimum depth = 2.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (root == null) return 0;
+    if (root.left == null && root.right == null) return 1;
+    if (root.left == null) return 1 + minDepth(root.right);
+    if (root.right == null) return 1 + minDepth(root.left);
+    return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+};
