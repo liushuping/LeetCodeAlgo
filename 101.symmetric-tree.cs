0f75c01c1ade3ff4a508547a7a1952fbca20cb9f
Shuping LIU <lsp.lhh@gmail.com>
2018-08-02 23:55:18 +0800

    problem 101

---
+++ 101.symmetric-tree.cs
@@ -0,0 +1,62 @@
+/*
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (41.07%)
+ * Total Accepted:    277.2K
+ * Total Submissions: 675K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * 
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * But the following [1,2,2,null,3,null,3]  is not:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public bool IsSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return IsSymmetric(root.left, root.right);
+    }
+    
+    public bool IsSymmetric(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null) return false;
+        if (node1.val != node2.val) return false;
+        return IsSymmetric(node1.right, node2.left) && IsSymmetric(node2.right, node1.left);
+    }
+}
