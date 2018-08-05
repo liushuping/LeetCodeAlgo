c389bd9bdac4ba7e08c022133251488cab731d59
Shuping LIU <lsp.lhh@gmail.com>
2018-08-05 22:50:58 +0800

    problem 176

---
+++ 176.second-highest-salary.sql
@@ -0,0 +1,38 @@
+#
+# [176] Second Highest Salary
+#
+# https://leetcode.com/problems/second-highest-salary/description/
+#
+# database
+# Easy (23.86%)
+# Total Accepted:    80.3K
+# Total Submissions: 336.3K
+# Testcase Example:  '{"headers": {"Employee": ["Id", "Salary"]}, "rows": {"Employee": [[1, 100], [2, 200], [3, 300]]}}'
+#
+# Write a SQL query to get the second highest salary from the Employee table.
+# 
+# 
+# +----+--------+
+# | Id | Salary |
+# +----+--------+
+# | 1  | 100    |
+# | 2  | 200    |
+# | 3  | 300    |
+# +----+--------+
+# 
+# 
+# For example, given the above Employee table, the query should return 200 as
+# the second highest salary. If there is no second highest salary, then the
+# query should return null.
+# 
+# 
+# +---------------------+
+# | SecondHighestSalary |
+# +---------------------+
+# | 200                 |
+# +---------------------+
+# 
+# 
+#
+# Write your MySQL query statement below
+select max(Salary) as SecondHighestSalary from Employee where Salary < (select max(Salary) from Employee)
