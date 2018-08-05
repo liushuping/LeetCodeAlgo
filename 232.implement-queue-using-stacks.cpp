9e2029bd43b5fe776d98751664fd7ba9dba07f39
Shuping LIU <lsp.lhh@gmail.com>
2018-08-06 02:33:21 +0800

    problem 232

---
+++ 232.implement-queue-using-stacks.cpp
@@ -0,0 +1,85 @@
+/*
+ * [232] Implement Queue using Stacks
+ *
+ * https://leetcode.com/problems/implement-queue-using-stacks/description/
+ *
+ * algorithms
+ * Easy (39.11%)
+ * Total Accepted:    114.3K
+ * Total Submissions: 292.2K
+ * Testcase Example:  '["MyQueue","push","push","peek","pop","empty"]\n[[],[1],[2],[],[],[]]'
+ *
+ * Implement the following operations of a queue using stacks.
+ * 
+ * 
+ * push(x) -- Push element x to the back of queue.
+ * pop() -- Removes the element from in front of queue.
+ * peek() -- Get the front element.
+ * empty() -- Return whether the queue is empty.
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * MyQueue queue = new MyQueue();
+ * 
+ * queue.push(1);
+ * queue.push(2);  
+ * queue.peek();  // returns 1
+ * queue.pop();   // returns 1
+ * queue.empty(); // returns false
+ * 
+ * Notes:
+ * 
+ * 
+ * You must use only standard operations of a stack -- which means only push to
+ * top, peek/pop from top, size, and is empty operations are valid.
+ * Depending on your language, stack may not be supported natively. You may
+ * simulate a stack by using a list or deque (double-ended queue), as long as
+ * you use only standard operations of a stack.
+ * You may assume that all operations are valid (for example, no pop or peek
+ * operations will be called on an empty queue).
+ * 
+ * 
+ */
+class MyQueue {
+private:
+    stack<int> s1, s2;
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        while(!empty()) s2.push(pop());
+        s1.push(x);
+        while(!s2.empty()) s1.push(s2.top()), s2.pop();
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int v = peek();
+        s1.pop();
+        return v;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        return s1.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * bool param_4 = obj.empty();
+ */
