f11ff8b4eddfdcd2a137bd74008c029d4e210b23
Shuping LIU <lsp.lhh@gmail.com>
2018-08-13 23:14:31 +0800

    problem 342

--- 342.power-of-four.cpp
+++ 342.power-of-four.cpp
@@ -33,6 +33,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        return num > 0 && !(num & num - 1 || num >> 1 & (num >> 1) - 1);
+        return num > 0 && !(num & num - 1) && num & 0x55555555;
     }
 };
