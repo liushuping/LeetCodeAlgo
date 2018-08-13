493d51edbde29f77ca4dbd20b234508018b7db8e
Shuping LIU <lsp.lhh@gmail.com>
2018-08-13 23:40:55 +0800

    problem 349

---
+++ 349.intersection-of-two-arrays.cpp
@@ -0,0 +1,48 @@
+/*
+ * [349] Intersection of Two Arrays
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (48.85%)
+ * Total Accepted:    148.3K
+ * Total Submissions: 303.6K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * Given two arrays, write a function to compute their intersection.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Each element in the result must be unique.
+ * The result can be in any order.
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s(nums1.begin(), nums1.end());
+        vector<int> res;
+        for(auto itr = nums2.begin(); itr != nums2.end(); ++itr) 
+            if (s.find(*itr) != s.end()) s.erase(*itr), res.push_back(*itr);
+        return res;
+    }
+};
